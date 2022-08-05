f429b8fcd9d4a511315dc0c6cc225bebe1e02969
Abdi-01 <abdialghi@gmail.com>
2022-08-05 14:01:21 +0700

    first commit

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -7,6 +7,7 @@ import io from 'socket.io-client';
 
 const API_URL = `http://localhost:4000`;
 const socket = io(API_URL);
+const socketNew = io(API_URL+'/notif');
 
 class ChatPage extends React.Component {
     constructor(props) {
